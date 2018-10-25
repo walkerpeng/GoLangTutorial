b5bb2cac7522aa68d2a6577e5757f46814f5a2c3
walkerpeng <jilin1257899@gmail.com>
2018-10-25 17:06:12 +0800

    20181025 commit

---
+++ VariadicFunction.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/**
+变体函数
+*/
+func sums(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	sums(1, 2)
+	sums(1, 2, 3)
+
+	nums := []int{1, 2, 3, 4}
+	sums(nums...)
+}
